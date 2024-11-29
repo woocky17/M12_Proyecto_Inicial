d22cc363fb8755a44f93117872db8e1be04ca8cd
Woocky17 <davidortetsabater@gmail.com>
2024-11-29 17:57:06 +0100

    Login component

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular';
+  somos = 'Marc, Antonio, David';
+
+  redirigirLogin(): void {
+    window.location.href = 'http://localhost:4200/login';
+  }
+  redirigirgetAll(): void {
+    window.location.href = 'http://localhost:4200/getAll';
+  }
+  redirigirfindOne(): void {
+    window.location.href = 'http://localhost:4200/findOne';
+  }
 }
