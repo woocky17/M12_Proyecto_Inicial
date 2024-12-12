d3ef4f34775979fc3a19e8d30c6c16864a5125d6
Antucan <antherpra@gmail.com>
2024-12-12 17:24:37 +0100

    no se muestra el nurse

--- Angular/src/app/find-nurse/find-nurse.component.ts
+++ Angular/src/app/find-nurse/find-nurse.component.ts
@@ -1,41 +1,18 @@
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import jsonData from '../data/data.json';
+import { nurseService } from '../service/antonio.service';
 
 @Component({
   selector: 'app-find-nurse',
   templateUrl: './find-nurse.component.html',
-  styleUrl: './find-nurse.component.css'
+  styleUrl: './find-nurse.component.css',
+  providers: [nurseService]
 })
 
-export class FindNurseComponent implements AfterViewInit {
+export class FindNurseComponent {
 
-  nurses: Nurse[] = jsonData;
-  @ViewChild("nameInput", { static: true }) NurseName!: ElementRef;
-  @ViewChild("nurseName", { static: false }) showNurseName!: ElementRef;
-  @ViewChild("nurseId", { static: false }) showNurseId!: ElementRef;
-  @ViewChild("nurseGmail", { static: false }) showNurseGmail!: ElementRef;
-
-  ngAfterViewInit() {
-    this.NurseName.nativeElement.value = '';
-  }
-
+  constructor(private nurseService: nurseService) { }
   findNurse() {
-    const inputName = this.NurseName.nativeElement.value.toLowerCase();
-    let found = false;
-    for (let nurse of this.nurses) {
-      if (inputName === nurse.name.toLowerCase()) {
-        this.showNurseId.nativeElement.innerHTML = nurse.id.toString();
-        this.showNurseName.nativeElement.innerHTML = nurse.name;
-        this.showNurseGmail.nativeElement.innerHTML = nurse.gmail;
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      this.showNurseId.nativeElement.innerText = "Not found!";
-      this.showNurseName.nativeElement.innerHTML = '';
-      this.showNurseGmail.nativeElement.innerHTML = '';
-    }
+    this.nurseService.findNurse();
   }
 }
 
--- Angular/src/app/service/antonio.service.ts
+++ Angular/src/app/service/antonio.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ViewChild, ElementRef } from '@angular/core';
 import jsonData from '../data/data.json';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Injectable()
 export class nurseService {
+    @ViewChild("nameInput", { static: true }) NurseName!: ElementRef;
+    @ViewChild("nurseName", { static: false }) showNurseName!: ElementRef;
+    @ViewChild("nurseId", { static: false }) showNurseId!: ElementRef;
+    @ViewChild("nurseGmail", { static: false }) showNurseGmail!: ElementRef;
+    ngAfterViewInit() {
+        this.NurseName.nativeElement.value = '';
+    }
     nurses: Nurse[] = jsonData;
     constructor(private _router: Router,
         private _activRoute: ActivatedRoute) { }
@@ -31,6 +38,25 @@ export class nurseService {
             this._router.navigate(['/getAll']);
         } else {
             alert('Invalid Gmail or Password');
+        }
+    }
+    findNurse() {
+
+        const inputName = this.NurseName.nativeElement.value.toLowerCase();
+        let found = false;
+        for (let nurse of this.nurses) {
+            if (inputName === nurse.name.toLowerCase()) {
+                this.showNurseId.nativeElement.innerHTML = nurse.id.toString();
+                this.showNurseName.nativeElement.innerHTML = nurse.name;
+                this.showNurseGmail.nativeElement.innerHTML = nurse.gmail;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            this.showNurseId.nativeElement.innerText = "Not found!";
+            this.showNurseName.nativeElement.innerHTML = '';
+            this.showNurseGmail.nativeElement.innerHTML = '';
         }
     }
 }
